56b34d1f6f24b482cb9c52ab6223de15220d0b9b
sedzinfo <sedzinfo@gmail.com>
2025-02-19 09:19:46 +0200

    functions

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 26 17:20:31 2017
-
 @author: Dimitrios Zacharatos
 """
 ##########################################################################################
@@ -26,8 +25,15 @@ from functions_excel import *
 ##########################################################################################
 # DATA
 ##########################################################################################
-df=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/blood_pressure.csv")
-personality=pd.read_csv(file_directory+"/data/personality.csv")
-titanic=pd.read_csv(file_directory+"/data/titanic.csv")
-
-check(df)
+df_blood_pressure=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/blood_pressure.csv")
+df_admission=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/admission.csv")
+df_automotive=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/automotive_data.csv")
+df_blood_pressure=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/blood_pressure.csv")
+df_crop_yield=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/crop_yield.csv")
+df_difficile=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/difficile.csv")
+df_insurance=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/insurance.csv")
+df_responses=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/responses.csv")
+df_responses_state=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/responses_state.csv")
+df_sexual_comp=pd.read_csv("https://raw.githubusercontent.com/researchpy/Data-sets/master/sexual_comp.csv")
+df_personality=pd.read_csv(file_directory+"/data/personality.csv")
+df_titanic=pd.read_csv(file_directory+"/data/titanic.csv")
--- functions/functions_check_dataframe.py
+++ functions/functions_check_dataframe.py
@@ -34,11 +34,37 @@ def check_integer(vector):
         if not is_int(i):
             return False
     return True
+# is_int(np.empty((10,)))
 # check_integer(np.empty((10,)))
 ##########################################################################################
 # SHORT CHECK DATAFRAME
 ##########################################################################################
 def short_check(df):
+    """
+    Performs a quick summary check of the given DataFrame (`df`) to provide basic statistics including the number of rows,
+    columns, empty strings, null values, and non-null values.
+
+    Parameters:
+    ----------
+    df : pandas.DataFrame
+        The DataFrame to analyze.
+
+    Returns:
+    -------
+    result : pandas.DataFrame
+        A DataFrame containing the following statistics for the provided DataFrame:
+        - COLUMNS: The number of columns in the DataFrame.
+        - ROWS: The number of rows in the DataFrame.
+        - EMPTY_STRINGS: The total count of empty string values in the DataFrame.
+        - NULLS: The total count of null (NaN) values in the DataFrame.
+        - NOT_NULLS: The total count of non-null (non-NaN) values in the DataFrame.
+
+    Notes:
+    ------
+    - The function calculates the number of rows and columns using the shape of the DataFrame.
+    - It counts null values using `isnull()`, empty strings using the comparison (`df == ""`), 
+      and non-null values using `count()`.
+    """
     dimensions=np.array(df.shape)
     rows=dimensions[0]
     collumns=dimensions[1]
@@ -51,13 +77,68 @@ def short_check(df):
                          'NULLS':[nulls],
                          'NOT_NULLS':[not_nulls]})
     return result
-# short_check(personality)
-# short_check(df)
-# short_check(titanic)
+# short_check(df_admission)
+# short_check(df_automotive)
+# short_check(df_blood_pressure)
+# short_check(df_crop_yield)
+# short_check(df_difficile)
+# short_check(df_insurance)
+# short_check(df_responses)
+# short_check(df_sexual_comp)
+# short_check(df_personality)
+# short_check(df_titanic)
 ##########################################################################################
 # CHECK DATAFRAME
 ##########################################################################################
-def check(df,filename=""):
+def cdf(df,width=10000,filename=""):
+    """
+    Performs a thorough check of the given DataFrame (`df`) and provides various statistics such as NULL, NAN, 
+    EMPTY_STRINGS, INFINITE, NOT_NULL, UNIQUE, MIN, MAX, MEAN, MEDIAN, RANGE, and SD for each column. Optionally,
+    the results can be saved to an Excel file. Additionally, the function temporarily adjusts pandas display settings 
+    to show the full content of the DataFrame.
+
+    Parameters:
+    ----------
+    df : pandas.DataFrame
+        The DataFrame to analyze.
+        
+    width : int, optional, default=10000
+        The width setting to be used for pandas display. This determines the column width when displaying the DataFrame.
+        
+    filename : str, optional, default=""
+        If provided, the function saves the results to an Excel file with two sheets: "SUMMARY" and "CHECK". If not provided, 
+        the results are not saved to a file.
+
+    Returns:
+    -------
+    check : pandas.DataFrame
+        A DataFrame containing statistical information for each column in `df` including NULLs, NANs, 
+        unique values, MIN, MAX, etc.
+        
+    short_check_result : pandas.DataFrame
+        A DataFrame returned by the `short_check` function, which provides additional insights into the DataFrame's structure.
+        
+    Notes:
+    ------
+    - The function temporarily modifies pandas display settings to allow for full viewing of the DataFrame columns and rows.
+    - The original display settings are restored after execution.
+    - This function uses helper functions `is_nan` and `is_inf` to check for NaN and infinite values in the DataFrame.
+    - If `filename` is provided, results are written to an Excel file using `xlsxwriter`.
+    """
+    
+    # Save the original display options
+    original_display_options = {
+        'max_columns': pd.get_option('display.max_columns'),
+        'max_rows': pd.get_option('display.max_rows'),
+        'max_colwidth': pd.get_option('display.max_colwidth'),
+        'width': pd.get_option('display.width'),
+        'max_seq_item': pd.get_option('display.max_seq_item')
+    }
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.width', width)
+    pd.set_option('display.max_seq_item', None)
     def is_nan(x):
         return x != x
     def is_inf(x):
@@ -67,9 +148,9 @@ def check(df,filename=""):
             return False
     short_check_result=short_check(df)
     sdf=pd.DataFrame({"NULL":df.isnull().sum(),
-                      "NAN":df.applymap(is_nan).sum(),
+                      "NAN":df.map(is_nan).sum(),
                       "EMPTY_STRINGS":(df=="").sum(),
-                      "INFINITE":df.applymap(is_inf).sum(),
+                      "INFINITE":df.map(is_inf).sum(),
                       "NOT_NULL":df.count(),
                       "UNIQUE":df.nunique(),
                       "MIN":df.min(axis=0,skipna=True,numeric_only=True),
@@ -77,7 +158,7 @@ def check(df,filename=""):
                       "MEAN":df.mean(axis=0,skipna=True,numeric_only=True),
                       "MEDIAN":df.median(axis=0,skipna=True,numeric_only=True),
                       "RANGE":df.max(axis=0,skipna=True,numeric_only=True)-df.min(axis=0,skipna=True,numeric_only=True),
-                      "SD":df.std(axis=None,skipna=True,ddof=1,numeric_only=True),
+                      "SD":df.std(axis=0,skipna=True,ddof=1,numeric_only=True),
                       "TYPE":df.dtypes
                       })
     check=sdf.reset_index()
@@ -86,16 +167,30 @@ def check(df,filename=""):
         critical_value_excel(pd.DataFrame(dataframes[0]),writer,"SUMMARY")
         critical_value_excel(pd.DataFrame(dataframes[1]),writer,"CHECK")
         writer.save()
-    return check, short_check_result
-# check(personality)
-# check(df)
-# check(titanic)
+    print(short_check_result)
+    print(check)
+    pd.set_option('display.max_columns', original_display_options['max_columns'])
+    pd.set_option('display.max_rows', original_display_options['max_rows'])
+    pd.set_option('display.max_colwidth', original_display_options['max_colwidth'])
+    pd.set_option('display.width', original_display_options['width'])
+    pd.set_option('display.max_seq_item', original_display_options['max_seq_item'])
+    # return check, short_check_result
+
+# cdf(df_admission)
+# cdf(df_automotive)
+# cdf(df_blood_pressure)
+# cdf(df_crop_yield)
+# cdf(df_difficile)
+# cdf(df_insurance)
+# cdf(df_responses)
+# cdf(df_sexual_comp)
+# cdf(df_personality)
+# cdf(df_titanic)
 ##########################################################################################
 # CHECK DATAFRAME ROWS
 ##########################################################################################
 def check_rows(df,filename=""):
-    dataframes=check(df.transpose(),filename=filename)
-    dataframes[1]=dataframes[1].dropna(axis=1,how='all')
+    dataframes=cdf(df.transpose(),filename=filename)
     return dataframes
 # check_rows(personality)
 # check_rows(df)
