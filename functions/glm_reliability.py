674a7d383351a7416a15b90bf2ccdb8a433c23a8
dimitrios <sedzinfo@gmail.com>
2025-04-27 09:20:56 +0300

    Update glm_reliability.py

--- functions/glm_reliability.py
+++ functions/glm_reliability.py
@@ -12,8 +12,8 @@ import sys
 import numpy as np
 import pandas as pd
 
-path_script = os.getcwd()
-path_root = path_script.replace('\\functions', '')
+path_script=os.getcwd()
+path_root=path_script.replace('\\functions', '')
 
 sys.path.insert(1,file_path)
 from __init__ import *
