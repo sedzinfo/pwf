75251968178f1ed9a5caa166a55db04aa7c68b29
sedzinfo <sedzinfo@gmail.com>
2025-05-06 17:26:34 +0300

    reliability

--- functions/glm_reliability.py
+++ functions/glm_reliability.py
@@ -30,40 +30,35 @@ ro.globalenv['df']=df_ocean.loc[:, df_ocean.columns.str.startswith("E")]
 ro.globalenv['df']=df_ocean.loc[:, df_ocean.columns.str.startswith("N")]
 
 traits=['O','C','E','A','N']
-keys_dict = {"O":[1,-1,1,-1,1,-1,1,1,1,1],
-             "C":[1,-1,1,-1,1,-1,1,-1,1,1],
-             "E":[1,-1,1,-1,1,-1,1,-1,1,-1],
-             "A":[-1,1,-1,1,-1,1,-1,1,1,1],
-             "N":[1,-1,1,-1,1,1,1,1,1,1]}
+keys_dict={"O":[1,-1,1,-1,1,-1,1,1,1,1],
+           "C":[1,-1,1,-1,1,-1,1,-1,1,1],
+           "E":[1,-1,1,-1,1,-1,1,-1,1,-1],
+           "A":[-1,1,-1,1,-1,1,-1,1,1,1],
+           "N":[1,-1,1,-1,1,1,1,1,1,1]}
 
 # alpha_result=ro.r('psych::alpha(df,check.keys=TRUE,n.iter=10)')
 
 alpha_result={}
+fa_result={}
 
 for trait in traits:
-    print(trait)
     keys="NULL"
     keys=keys_dict[trait]
-    str_with_parentheses = "(" + ", ".join(str(x) for x in keys) + ")"
+    str_with_parentheses="(" + ", ".join(str(x) for x in keys) + ")"
     psych_string='psych::alpha(df,'+'keys=c'+str_with_parentheses+',n.iter=10)'
-    df_trait=df_ocean.loc[:, df_ocean.columns.str.startswith(trait)]
+    df_trait=df_ocean.loc[:,df_ocean.columns.str.startswith(trait)]
+    fa_result[trait]=FactorAnalyzer(rotation="varimax")
+    fa_result[trait].fit(df_trait)
     ro.globalenv['df']=df_trait
     result=ro.r(psych_string)
     alpha_result[trait]=result
 
-trait='N'
-
-for trait in traits:
-    print(alpha_result[trait])
-
-print(alpha_result[trait].names)
-print(alpha_result[trait])
-
-result_total_summary_list = []
-result_items_list = []
+result_total_summary_list=[]
+result_items_list=[]
 
 for trait in traits:
     print(f"Trait: {trait}")
+    eigen_values,vectors=fa_result[trait].get_eigenvalues()
     r_total=alpha_result[trait].rx2('total')
     r_alpha_drop=alpha_result[trait].rx2('alpha.drop')
     r_item_stats=alpha_result[trait].rx2('item.stats')
@@ -94,12 +89,14 @@ for trait in traits:
     result_boot_ci=pd.DataFrame(pd.DataFrame(r_boot_ci).T)
     result_boot_ci.columns=['ci_lower','ci','ci_upper']
     result_boot=pd.DataFrame(r_boot)
-    result_nvar=pd.DataFrame(r_var_r,columns=["nvar"])
+    result_var=pd.DataFrame(r_var_r,columns=["nvar"])
+    result_eigen=pd.DataFrame([[np.sum(eigen_values>1)]],columns=["kaiser"])
 
     item_names=pd.DataFrame(result_item_stats.index,columns=["item"])
 
     temp_result_total_summary=pd.concat([pd.DataFrame({'trait':[trait]*len(result_nvar)}),
-                                         result_nvar,
+                                         result_var,
+                                         result_eigen,
                                          result_total.reset_index(drop=True),
                                          result_boot_ci.reset_index(drop=True),
                                          result_keys.reset_index(drop=True)],
@@ -118,37 +115,21 @@ for trait in traits:
     result_total_summary=pd.concat(result_total_summary_list, ignore_index=True, sort=False)
     result_items=pd.concat(result_items_list, ignore_index=True, sort=False)
 
-
-
-
-
-
-print(alpha_result.rx2('total'))
-print(alpha_result.rx2('alpha.drop'))
-print(alpha_result.rx2('item.stats'))
-print(alpha_result.rx2('response.freq'))
-print(alpha_result.rx2('keys'))
-print(alpha_result.rx2('scores'))
-print(alpha_result.rx2('nvar'))
-print(alpha_result.rx2('boot.ci'))
-print(alpha_result.rx2('boot'))
-print(alpha_result.rx2('feldt'))
-print(alpha_result.rx2('Unidim'))
-print(alpha_result.rx2('var.r'))
-print(alpha_result.rx2('Fit'))
-print(alpha_result.rx2('call'))
-print(alpha_result.rx2('title'))
-
-alpha_result.rx2('call')
-
-alpha_result.rx2('call')
-
-alpha_result.rx2('total')
-alpha_result.do_slot_assign
-alpha_result.from_iterable
-alpha_result.from_length
-alpha_result.from_memoryview
-
-print(alpha_result)
-
-
+for trait in traits:
+    print(alpha_result[trait])
+    print(alpha_result[trait].names)
+    print(alpha_result[trait].rx2('total'))
+    print(alpha_result[trait].rx2('alpha.drop'))
+    print(alpha_result[trait].rx2('item.stats'))
+    print(alpha_result[trait].rx2('response.freq'))
+    print(alpha_result[trait].rx2('keys'))
+    print(alpha_result[trait].rx2('scores'))
+    print(alpha_result[trait].rx2('nvar'))
+    print(alpha_result[trait].rx2('boot.ci'))
+    print(alpha_result[trait].rx2('boot'))
+    print(alpha_result[trait].rx2('feldt'))
+    print(alpha_result[trait].rx2('Unidim'))
+    print(alpha_result[trait].rx2('var.r'))
+    print(alpha_result[trait].rx2('Fit'))
+    print(alpha_result[trait].rx2('call'))
+    print(alpha_result[trait].rx2('title'))
